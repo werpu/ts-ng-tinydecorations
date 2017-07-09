f0a08ae3b82820bcb399600cd2a74ceff3c7a266
Werner Punz <werner.punz@gmail.com>
2017-07-09 21:34:41 +0200

    mixed param and post services working

---
+++ example/app/typescript/testAssets/RestService2.js
@@ -0,0 +1,34 @@
+System.register(["TinyDecorations"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var __moduleName = context_1 && context_1.id;
+    var TinyDecorations_1, RestService2;
+    return {
+        setters: [
+            function (TinyDecorations_1_1) {
+                TinyDecorations_1 = TinyDecorations_1_1;
+            }
+        ],
+        execute: function () {
+            RestService2 = (function () {
+                function RestService2() {
+                }
+                RestService2 = __decorate([
+                    TinyDecorations_1.Injectable({ name: "RestService2" }),
+                    __metadata("design:paramtypes", [])
+                ], RestService2);
+                return RestService2;
+            }());
+            exports_1("RestService2", RestService2);
+        }
+    };
+});
+//# sourceMappingURL=RestService2.js.map
